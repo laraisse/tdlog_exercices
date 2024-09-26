966eefbaabb89721b5bf5b73be129c94e271fb42
laraisse <laraisse66@gmail.com>
2024-09-26 11:41:19 +0100

    exo2

--- exo2/exo2.py
+++ exo2/exo2.py
@@ -28,3 +28,32 @@ fixed_tests_False = (
     ( "spam",    "eggs"  )
 )
 """
+def string_test(arg1,arg2):
+    if arg1[len(arg1)-len(arg2):] == arg2 :
+        return True
+    else:
+        return False
+
+fixed_tests_True = (
+    ( "samurai", "ai"    ),
+    ( "ninja",   "ja"    ),
+    ( "sensei",  "i"     ),
+    ( "abc",     "abc"   ),
+    ( "abcabc",  "bc"    ),
+    ( "fails",   "ails"  ),
+)
+
+fixed_tests_False = (
+    ( "sumo",    "omo"   ),
+    ( "samurai", "ra"    ),
+    ( "abc",     "abcd"  ),
+    ( "ails",    "fails" ),
+    ( "this",    "fails" ),
+    ( "spam",    "eggs"  )
+)
+
+for i in fixed_tests_False:
+    print(string_test(i[0],i[1]))
+
+for i in fixed_tests_True:
+    print(string_test(i[0],i[1]))
